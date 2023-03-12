38cf0360e0aa62930e4e6e2d8c1df54d24e8f39b
Mike_Belyakov <48880903+VODOROD1@users.noreply.github.com>
2023-03-12 16:25:20 +0400

    Styles has been moved in special scss file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+interface Priority {
+  id: number,
+  title: string,
+  color: string
+}
+
+interface Category {
+  id: number,
+  title: string
+}
+
+interface Task {
+  id: number,
+  title: string,
+  status: boolean,
+  priority: Priority,
+  category: Category,
+  term: Date
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'todo-app';
+
+
 }
