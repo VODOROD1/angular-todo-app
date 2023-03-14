796922a740c3d53504d10014e178e9d6c752cefd
Mike_Belyakov <48880903+VODOROD1@users.noreply.github.com>
2023-03-14 12:03:48 +0400

    Data from TestData were engaged in categories component thanks to data-handler service

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Category } from 'src/app/model/Category';
+import { DataHandlerService } from 'src/app/service/data-handler.service';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
 })
 export class CategoriesComponent {
+  public categories: Category[] = [];
 
+  constructor(private dataHandlerService: DataHandlerService) {}
+
+  ngOnInit() {
+    this.categories = this.dataHandlerService.getCategories();
+  }
 }
