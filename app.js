d5ecae3e928c14a37a36fb8b5069ba84a135196e
Mike_Belyakov <48880903+VODOROD1@users.noreply.github.com>
2023-03-17 21:12:15 +0400

    POST and GET method were added for backend

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 /*
  * Получение GET и POST запросов
  */
-const http = require("http");
-const url = require("url");
+const http = require("http")
+const url = require("url")
+// const {parseQuerystring} = require("querystring")
+const querystring = require('node:querystring');
 
 http
   .createServer((request, response) => {
     if (request.method === "GET") {
-      console.log(request.method);
-      const urlRequest = url.parse(request.url, true);
-      console.log("urlRequest", urlRequest);
-      console.log(urlRequest.query.test);
+      console.log(request.method)
+      const urlRequest = url.parse(request.url, true)
+      console.log("urlRequest", urlRequest)
+      console.log(urlRequest.query.test)
       if (urlRequest.query.test % 2 === 0) {
-        response.end("AAAAA");
+        response.end("AAAAA")
       } else {
-        response.end("BBBBB");
+        response.end("BBBBB")
       }
     } else if (request.method === "POST") {
-
+        let body = ''
+        request.on('data', chunk => {
+            body += chunk.toString()
+        })
+        request.on('end', () => {
+            console.log('body',body)
+            let params = querystring.parse(body)
+            console.log("params",params)
+            response.end('ok')
+        })
     }
   })
-  .listen(3000);
+  .listen(3000)
