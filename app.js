33d842748bb22ce69d51acc9d76c003dc1cc4acd
Mike_Belyakov <48880903+VODOROD1@users.noreply.github.com>
2023-03-17 19:44:06 +0400

    Simple logic for get-query was added

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-/* 
-* Получение GET и POST запросов
-*/
-const http = require("http")
-http.createServer((request, response) => {
-    response.end('gooo')
-}).listen(3000)
+/*
+ * Получение GET и POST запросов
+ */
+const http = require("http");
+const url = require("url");
 
+http
+  .createServer((request, response) => {
+    if (request.method === "GET") {
+      console.log(request.method);
+      const urlRequest = url.parse(request.url, true);
+      console.log("urlRequest", urlRequest);
+      console.log(urlRequest.query.test);
+      if (urlRequest.query.test % 2 === 0) {
+        response.end("AAAAA");
+      } else {
+        response.end("BBBBB");
+      }
+    } else if (request.method === "POST") {
 
+    }
+  })
+  .listen(3000);
